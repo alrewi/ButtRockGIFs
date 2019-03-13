680da7a34042800102c11172ac19192e23d4abd2
Alexandra Wilson <alexandrareginawilson@gmail.com>
2019-03-12 21:26:47 -0500

    finished most HTML and CSS, still battling 2 JS bugs

---
+++ javascript/generator.js
@@ -0,0 +1,93 @@
+var rockers = [
+                "def leppard", 
+                "motley crue", 
+                "whitesnake", 
+                "van halen", 
+                "bon jovi",
+                "skid row", 
+                "poison", 
+                "zz top", 
+                "guns 'n' roses", 
+                "ac/dc",
+                "warrant"
+            ];
+
+//We need to create a button for each element in the rockers array and push it in to the .btn-grp div
+function makeButtons () {
+    //Prevent buttons from repeating when adding new ones
+    $(".btn-grp").empty();
+    //Create the buttons with a class, attribute and text
+    for (var i = 0; i < rockers.length; i++) {
+        var rockBand = $("<button/>");
+        rockBand.addClass("rockButton");
+        rockBand.attr("data-name", rockers[i]);
+        rockBand.text(rockers[i]);
+        
+        $(".btn-grp").append(rockBand);
+    }
+    displayGifs();
+}
+makeButtons();
+
+//Adding a new band to the list of buttons using the html form
+$("#add-band").on("click", function (event) {
+    event.preventDefault();
+    
+    var band = $("#band-input").val().trim(); 
+    rockers.push(band);
+    
+    makeButtons();
+})
+
+//Each time a button is clicked, an ajax request must be sent to the giphy api to retrieve the gifs
+//and the gifs must be displayed in the gif-display
+function displayGifs() {
+
+    $(document).on("click", ".rockButton", function () {
+        //Every time a new button is clicked, the old gifs are replaced
+        $("#gif-display").empty();
+
+        var band = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=lvgRYkPfs3HiNOgsh1814LVILJAjHmRx&limit=10&q=" + band;
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            for (var i = 0; i < response.data.length; i++) {
+                var gifDiv = $("<div>");
+
+                var gif = $("<img>");
+                gif.addClass('gif');
+                gif.attr('src', response.data[i].images.fixed_height_still.url);
+                gif.attr('data-still', response.data[i].images.fixed_height_still.url);
+                gif.attr('data-animate', response.data[i].images.fixed_height.url);
+                gif.attr('data-state', "still");
+                
+                gifDiv.append(gif);
+
+                var rating = response.data[i].rating;
+                var ratingDiv = $("<p class='rating'>").text("Rating: " + rating);
+                
+                gifDiv.append(ratingDiv);
+                
+                $("#gif-display").prepend(gifDiv);
+            }
+        })
+    })
+}
+
+//If a user clicks a gif, it should start playing
+//If it's clicked a second time, it should stop.
+$(".gif").on("click", function () {
+            
+    var state = $(this).attr("data-state");
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+
